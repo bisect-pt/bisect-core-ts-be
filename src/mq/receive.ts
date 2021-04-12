6d16fd408762c722949d2bb8eabb7a7581c514ac
Pedro Ferreira <pedro@bisect.pt>
2021-04-12 16:41:14 +0100

    Update dependencies (#5)

--- src/mq/receive.ts
+++ src/mq/receive.ts
@@ -1,12 +1,12 @@
-import * as EventEmitter from 'events';
-import { logger } from '../../../bisect-core-ts/src/logger';
-import { setupChannel, ChannelErrorCallback } from './common';
-import { ConsumeMessage } from 'amqplib';
-import * as types from './types';
+import * as EventEmitter from "events";
+import { logger } from "@bisect/bisect-core-ts";
+import { setupChannel, ChannelErrorCallback } from "./common";
+import { ConsumeMessage } from "amqplib";
+import * as types from "./types";
 
 type RawMessage = {
-    msg: Buffer;
-    ack: () => void;
+  msg: Buffer;
+  ack: () => void;
 };
 
 type RawMessageCallback = (m: RawMessage) => void;
@@ -17,153 +17,186 @@ type BaseReceiverType = { close: () => void };
     onMessageCallback: ack MUST be called to confirm that the message has been processed.
 */
 const doCreateQueueReceiver = async (
-    brokerUrl: string,
-    queue: types.IQueueInfo,
-    onMessageCallback: RawMessageCallback,
-    onChannelErrorCallback: ChannelErrorCallback
+  brokerUrl: string,
+  queue: types.IQueueInfo,
+  onMessageCallback: RawMessageCallback,
+  onChannelErrorCallback: ChannelErrorCallback
 ): Promise<BaseReceiverType> => {
-    const { channel, close } = await setupChannel(brokerUrl, onChannelErrorCallback);
+  const { channel, close } = await setupChannel(
+    brokerUrl,
+    onChannelErrorCallback
+  );
 
-    await channel.assertQueue(queue.name, queue.options);
+  await channel.assertQueue(queue.name, queue.options);
 
-    channel.prefetch(1);
-    logger.info(` [*] Waiting for messages in ${queue.name}`);
+  channel.prefetch(1);
+  logger.info(` [*] Waiting for messages in ${queue.name}`);
 
-    const onMessage = (m: ConsumeMessage | null) => {
-        if (!m) return;
+  const onMessage = (m: ConsumeMessage | null) => {
+    if (!m) return;
 
-        onMessageCallback({
-            msg: m.content,
-            ack: () => channel.ack(m),
-        });
-    };
-
-    await channel.consume(queue.name, onMessage, {
-        noAck: false,
+    onMessageCallback({
+      msg: m.content,
+      ack: () => channel.ack(m),
     });
+  };
 
-    return {
-        close: close,
-    };
+  await channel.consume(queue.name, onMessage, {
+    noAck: false,
+  });
+
+  return {
+    close: close,
+  };
 };
 
 /* 
     onMessageCallback: ack MUST be called to confirm that the message has been processed.
 */
 const doCreateExchangeReceiver = async (
-    brokerUrl: string,
-    exchangeInfo: types.IExchangeInfo,
-    topics: string[],
-    onMessageCallback: RawMessageCallback,
-    onChannelErrorCallback: ChannelErrorCallback
+  brokerUrl: string,
+  exchangeInfo: types.IExchangeInfo,
+  topics: string[],
+  onMessageCallback: RawMessageCallback,
+  onChannelErrorCallback: ChannelErrorCallback
 ): Promise<BaseReceiverType> => {
-    const { channel, close } = await setupChannel(brokerUrl, onChannelErrorCallback);
+  const { channel, close } = await setupChannel(
+    brokerUrl,
+    onChannelErrorCallback
+  );
 
-    await channel.assertExchange(exchangeInfo.name, exchangeInfo.type, exchangeInfo.options);
+  await channel.assertExchange(
+    exchangeInfo.name,
+    exchangeInfo.type,
+    exchangeInfo.options
+  );
 
-    const queueName = ''; // unnamed queue
-    const queueOptions = { exclusive: true }; // unnamed queue
-    const q = await channel.assertQueue(queueName, queueOptions);
+  const queueName = ""; // unnamed queue
+  const queueOptions = { exclusive: true }; // unnamed queue
+  const q = await channel.assertQueue(queueName, queueOptions);
 
-    topics.forEach(async (key) => await channel.bindQueue(q.queue, exchangeInfo.name, key));
+  topics.forEach(
+    async (key) => await channel.bindQueue(q.queue, exchangeInfo.name, key)
+  );
 
-    channel.prefetch(1);
-    logger.info(` [*] Waiting for messages in exchange ${exchangeInfo.name}`);
+  channel.prefetch(1);
+  logger.info(` [*] Waiting for messages in exchange ${exchangeInfo.name}`);
 
-    const onMessage = (m: ConsumeMessage | null) => {
-        if (!m) return;
+  const onMessage = (m: ConsumeMessage | null) => {
+    if (!m) return;
 
-        onMessageCallback({
-            msg: m.content,
-            ack: () => channel.ack(m),
-        });
-    }
-
-    await channel.consume(q.queue, onMessage, {
-        noAck: false,
+    onMessageCallback({
+      msg: m.content,
+      ack: () => channel.ack(m),
     });
+  };
 
-    return {
-        close: close,
-    };
+  await channel.consume(q.queue, onMessage, {
+    noAck: false,
+  });
+
+  return {
+    close: close,
+  };
 };
 
 function createGenericReceiver<
-    ReceiverType extends BaseReceiverType,
-    CreatorType extends (m: RawMessageCallback, e: ChannelErrorCallback) => Promise<ReceiverType>
+  ReceiverType extends BaseReceiverType,
+  CreatorType extends (
+    m: RawMessageCallback,
+    e: ChannelErrorCallback
+  ) => Promise<ReceiverType>
 >(creator: CreatorType) {
-    const emitter = new EventEmitter.EventEmitter();
-    const onMessage = (m: RawMessage) => {
-        emitter.emit(onMessageKey, m.msg);
-        m.ack();
-    };
+  const emitter = new EventEmitter.EventEmitter();
+  const onMessage = (m: RawMessage) => {
+    emitter.emit(onMessageKey, m.msg);
+    m.ack();
+  };
 
-    let receiver: ReceiverType | null = null;
+  let receiver: ReceiverType | null = null;
 
-    const healthChecker = async () => {
-        if (receiver !== null || creatingNow) {
-            return;
-        }
+  const healthChecker = async () => {
+    if (receiver !== null || creatingNow) {
+      return;
+    }
 
-        logger.info('Health checker: trying to create receiver');
-        try {
-            creatingNow = true;
-            receiver = await creator(onMessage, onChannelError);
-        } catch (err) {
-            logger.error(`Error connecting to AMQP broker: ${err}`);
-            receiver = null;
-        } finally {
-            creatingNow = false;
-        }
-    };
+    logger.info("Health checker: trying to create receiver");
+    try {
+      creatingNow = true;
+      receiver = await creator(onMessage, onChannelError);
+    } catch (err) {
+      logger.error(`Error connecting to AMQP broker: ${err}`);
+      receiver = null;
+    } finally {
+      creatingNow = false;
+    }
+  };
 
-    let healthCheckerTimerId: NodeJS.Timeout | null = null;
-    const healthCheckInterval = 1000;
-    let creatingNow = false;
+  let healthCheckerTimerId: NodeJS.Timeout | null = null;
+  const healthCheckInterval = 1000;
+  let creatingNow = false;
 
-    const startHealthChecker = () => {
-        if (healthCheckerTimerId !== null) {
-            return;
-        }
+  const startHealthChecker = () => {
+    if (healthCheckerTimerId !== null) {
+      return;
+    }
 
-        healthCheckerTimerId = setInterval(healthChecker, healthCheckInterval);
-    };
+    healthCheckerTimerId = setInterval(healthChecker, healthCheckInterval);
+  };
 
-    const onChannelError = () => {
-        logger.error('Channel error');
-        receiver = null;
-    };
+  const onChannelError = () => {
+    logger.error("Channel error");
+    receiver = null;
+  };
 
-    const doClose = () => {
-        if (healthCheckerTimerId) {
-            clearInterval(healthCheckerTimerId);
-        }
-        healthCheckerTimerId = null;
-        if (receiver) {
-            receiver.close();
-        }
-    };
+  const doClose = () => {
+    if (healthCheckerTimerId) {
+      clearInterval(healthCheckerTimerId);
+    }
+    healthCheckerTimerId = null;
+    if (receiver) {
+      receiver.close();
+    }
+  };
 
-    startHealthChecker();
+  startHealthChecker();
 
-    return {
-        emitter,
-        close: doClose,
-    };
+  return {
+    emitter,
+    close: doClose,
+  };
 }
 
-export const createQueueReceiver = (brokerUrl: string, queue: types.IQueueInfo) => {
-    const creator = async (onMessage: RawMessageCallback, onChannelError: ChannelErrorCallback) =>
-        doCreateQueueReceiver(brokerUrl, queue, onMessage, onChannelError);
+export const createQueueReceiver = (
+  brokerUrl: string,
+  queue: types.IQueueInfo
+) => {
+  const creator = async (
+    onMessage: RawMessageCallback,
+    onChannelError: ChannelErrorCallback
+  ) => doCreateQueueReceiver(brokerUrl, queue, onMessage, onChannelError);
 
-    return createGenericReceiver(creator);
+  return createGenericReceiver(creator);
 };
 
-export const createExchangeReceiver = (brokerUrl: string, exchangeInfo: types.IExchangeInfo, topics: string[]) => {
-    const creator = async (onMessage: RawMessageCallback, onChannelError: ChannelErrorCallback) =>
-        doCreateExchangeReceiver(brokerUrl, exchangeInfo, topics, onMessage, onChannelError);
+export const createExchangeReceiver = (
+  brokerUrl: string,
+  exchangeInfo: types.IExchangeInfo,
+  topics: string[]
+) => {
+  const creator = async (
+    onMessage: RawMessageCallback,
+    onChannelError: ChannelErrorCallback
+  ) =>
+    doCreateExchangeReceiver(
+      brokerUrl,
+      exchangeInfo,
+      topics,
+      onMessage,
+      onChannelError
+    );
 
-    return createGenericReceiver(creator);
+  return createGenericReceiver(creator);
 };
 
-export const onMessageKey = 'onMessage';
+export const onMessageKey = "onMessage";
--- src/mq/send.ts
+++ src/mq/send.ts
@@ -1,193 +1,225 @@
-import { logger } from '../../../bisect-core-ts/src/logger';
-import * as types from './types';
-import { setupChannel } from './common';
+import { logger } from "@bisect/bisect-core-ts";
+import * as types from "./types";
+import { setupChannel } from "./common";
 
 interface IQueuePipe {
-    type: 'queue';
-    send: (m: types.AnyQueueMessage) => void;
-    close: () => void;
+  type: "queue";
+  send: (m: types.AnyQueueMessage) => void;
+  close: () => void;
 }
 
 interface IExchangePipe {
-    type: 'exchange';
-    send: (m: types.ExchangeMessage) => void;
-    close: () => void;
+  type: "exchange";
+  send: (m: types.ExchangeMessage) => void;
+  close: () => void;
 }
 
 type TargetTypes = types.IQueueInfo | types.IExchangeInfo;
-type PipeCreator<TargetType, PipeType> = (brokerUrl: string, queue: TargetType) => Promise<PipeType>;
+type PipeCreator<TargetType, PipeType> = (
+  brokerUrl: string,
+  queue: TargetType
+) => Promise<PipeType>;
 
 type GenericSenderType<MessageType> = {
-    send: (m: MessageType) => void;
-    close: () => void;
+  send: (m: MessageType) => void;
+  close: () => void;
 };
 
-const createQueuePipe = async (brokerUrl: string, queue: types.IQueueInfo): Promise<IQueuePipe> => {
-    const { channel, close } = await setupChannel(brokerUrl);
+const createQueuePipe = async (
+  brokerUrl: string,
+  queue: types.IQueueInfo
+): Promise<IQueuePipe> => {
+  const { channel, close } = await setupChannel(brokerUrl);
 
-    await channel.assertQueue(queue.name, queue.options);
+  await channel.assertQueue(queue.name, queue.options);
 
-    const send = (m: types.AnyQueueMessage) => {
-        const { msg, persistent } = m;
-        channel.sendToQueue(queue.name, Buffer.from(JSON.stringify(msg)), {
-            persistent: persistent,
-        });
-    };
+  const send = (m: types.AnyQueueMessage) => {
+    const { msg, persistent } = m;
+    channel.sendToQueue(queue.name, Buffer.from(JSON.stringify(msg)), {
+      persistent: persistent,
+    });
+  };
 
-    return {
-        type: 'queue',
-        send,
-        close,
-    };
+  return {
+    type: "queue",
+    send,
+    close,
+  };
 };
 
-const createExchangePipe = async (brokerUrl: string, exchange: types.IExchangeInfo): Promise<IExchangePipe> => {
-    const { channel, close } = await setupChannel(brokerUrl);
+const createExchangePipe = async (
+  brokerUrl: string,
+  exchange: types.IExchangeInfo
+): Promise<IExchangePipe> => {
+  const { channel, close } = await setupChannel(brokerUrl);
 
-    channel.assertExchange(exchange.name, exchange.type, exchange.options);
+  channel.assertExchange(exchange.name, exchange.type, exchange.options);
 
-    const send = (m: types.ExchangeMessage): void => {
-        const { key, msg } = m;
-        channel.publish(exchange.name, key, Buffer.from(JSON.stringify(msg)));
-    };
+  const send = (m: types.ExchangeMessage): void => {
+    const { key, msg } = m;
+    channel.publish(exchange.name, key, Buffer.from(JSON.stringify(msg)));
+  };
 
-    return {
-        type: 'exchange',
-        send,
-        close,
-    };
+  return {
+    type: "exchange",
+    send,
+    close,
+  };
 };
 
-class GenericSender<MessageType, SenderType extends GenericSenderType<MessageType>, TargetType> {
-    private sender: SenderType | null = null;
-    constructor(private readonly pipeCreator: PipeCreator<TargetType, SenderType>) {}
+class GenericSender<
+  MessageType,
+  SenderType extends GenericSenderType<MessageType>,
+  TargetType
+> {
+  private sender: SenderType | null = null;
+  constructor(
+    private readonly pipeCreator: PipeCreator<TargetType, SenderType>
+  ) {}
 
-    public async send(brokerUrl: string, target: TargetType, content: MessageType): Promise<boolean> {
-        try {
-            if (!this.sender) {
-                this.sender = await this.pipeCreator(brokerUrl, target);
-            }
+  public async send(
+    brokerUrl: string,
+    target: TargetType,
+    content: MessageType
+  ): Promise<boolean> {
+    try {
+      if (!this.sender) {
+        this.sender = await this.pipeCreator(brokerUrl, target);
+      }
 
-            if (this.sender) {
-                this.sender.send(content);
-            }
+      if (this.sender) {
+        this.sender.send(content);
+      }
 
-            return true;
-        } catch (err) {
-            logger.error(`Error sending to target: ${err.message}`);
+      return true;
+    } catch (err) {
+      logger.error(`Error sending to target: ${err.message}`);
 
-            this.sender = null;
-        }
-
-        return false;
+      this.sender = null;
     }
 
-    public close() {
-        this.sender?.close();
-    }
+    return false;
+  }
+
+  public close() {
+    this.sender?.close();
+  }
 }
 
 function createGenericSender<
-    MessageType,
-    SenderType extends GenericSenderType<MessageType>,
-    TargetType extends TargetTypes
->(pipeCreator: PipeCreator<TargetType, SenderType>, brokerUrl: string, target: TargetType) {
-    const sender = new GenericSender<MessageType, SenderType, TargetType>(pipeCreator);
-    const outputQueue: MessageType[] = [];
-    const isDurable: boolean = target.options?.durable === true;
+  MessageType,
+  SenderType extends GenericSenderType<MessageType>,
+  TargetType extends TargetTypes
+>(
+  pipeCreator: PipeCreator<TargetType, SenderType>,
+  brokerUrl: string,
+  target: TargetType
+) {
+  const sender = new GenericSender<MessageType, SenderType, TargetType>(
+    pipeCreator
+  );
+  const outputQueue: MessageType[] = [];
+  const isDurable: boolean = target.options?.durable === true;
 
-    const retrySend = async () => {
-        logger.info(`Retrying to send. Output queue size: ${outputQueue.length}`);
+  const retrySend = async () => {
+    logger.info(`Retrying to send. Output queue size: ${outputQueue.length}`);
 
-        while (outputQueue.length > 0) {
-            const content = outputQueue.shift();
+    while (outputQueue.length > 0) {
+      const content = outputQueue.shift();
 
-            if (content) {
-                if (!(await sender.send(brokerUrl, target, content))) {
-                    outputQueue.unshift(content);
-                    return;
-                }
-            }
+      if (content) {
+        if (!(await sender.send(brokerUrl, target, content))) {
+          outputQueue.unshift(content);
+          return;
         }
+      }
+    }
 
-        stopRetryTimer();
-    };
+    stopRetryTimer();
+  };
 
-    let retryTimerId: NodeJS.Timeout | null = null;
-    const retryInterval = 1000;
+  let retryTimerId: NodeJS.Timeout | null = null;
+  const retryInterval = 1000;
 
-    const startRetryTimer = () => {
-        if (retryTimerId !== null) {
-            return;
-        }
+  const startRetryTimer = () => {
+    if (retryTimerId !== null) {
+      return;
+    }
 
-        logger.info('Starting retry timer');
+    logger.info("Starting retry timer");
 
-        retryTimerId = setInterval(retrySend, retryInterval);
-    };
+    retryTimerId = setInterval(retrySend, retryInterval);
+  };
 
-    const stopRetryTimer = () => {
-        if (retryTimerId === null) {
-            return;
-        }
+  const stopRetryTimer = () => {
+    if (retryTimerId === null) {
+      return;
+    }
 
-        logger.info('Stopping retry timer');
+    logger.info("Stopping retry timer");
 
-        clearInterval(retryTimerId);
-        retryTimerId = null;
-    };
+    clearInterval(retryTimerId);
+    retryTimerId = null;
+  };
 
-    const isQueueFull = () => {
-        return outputQueue.length > types.MAX_OUTSTANDING_MESSAGES;
-    };
+  const isQueueFull = () => {
+    return outputQueue.length > types.MAX_OUTSTANDING_MESSAGES;
+  };
 
-    const send = async (content: MessageType) => {
-        if (outputQueue.length > 0) {
-            if (isQueueFull()) {
-                const message = 'Error sending message. Discarded.';
-                logger.error(message);
-                throw new Error(message);
-            } else {
-                outputQueue.push(content);
-            }
-            return;
-        }
+  const send = async (content: MessageType) => {
+    if (outputQueue.length > 0) {
+      if (isQueueFull()) {
+        const message = "Error sending message. Discarded.";
+        logger.error(message);
+        throw new Error(message);
+      } else {
+        outputQueue.push(content);
+      }
+      return;
+    }
 
-        if (await sender.send(brokerUrl, target, content)) {
-            return;
-        }
+    if (await sender.send(brokerUrl, target, content)) {
+      return;
+    }
 
-        // Failed to send. Try to save the message, if required
-        if (isDurable && !isQueueFull()) {
-            outputQueue.push(content);
-            logger.error('Error sending message. Saving in memory.');
+    // Failed to send. Try to save the message, if required
+    if (isDurable && !isQueueFull()) {
+      outputQueue.push(content);
+      logger.error("Error sending message. Saving in memory.");
 
-            startRetryTimer();
-        } else {
-            logger.error('Error sending message. Discarded.');
-        }
-    };
+      startRetryTimer();
+    } else {
+      logger.error("Error sending message. Discarded.");
+    }
+  };
 
-    return {
-        send,
-        close: () => {
-            // TODO: close does not make sure that all messages are sent.
-            stopRetryTimer();
-            if (sender) {
-                sender.close();
-            }
-        },
-    };
+  return {
+    send,
+    close: () => {
+      // TODO: close does not make sure that all messages are sent.
+      stopRetryTimer();
+      if (sender) {
+        sender.close();
+      }
+    },
+  };
 }
 
-export const createExchangeSender = (brokerUrl: string, exchange: types.IExchangeInfo) =>
-    createGenericSender<types.ExchangeMessage, IExchangePipe, types.IExchangeInfo>(
-        createExchangePipe,
-        brokerUrl,
-        exchange
-    );
+export const createExchangeSender = (
+  brokerUrl: string,
+  exchange: types.IExchangeInfo
+) =>
+  createGenericSender<
+    types.ExchangeMessage,
+    IExchangePipe,
+    types.IExchangeInfo
+  >(createExchangePipe, brokerUrl, exchange);
 export const createQueueSender = (brokerUrl: string, queue: types.IQueueInfo) =>
-    createGenericSender<types.AnyQueueMessage, IQueuePipe, types.IQueueInfo>(createQueuePipe, brokerUrl, queue);
+  createGenericSender<types.AnyQueueMessage, IQueuePipe, types.IQueueInfo>(
+    createQueuePipe,
+    brokerUrl,
+    queue
+  );
 
 export const persistent = true;
 export const notPersistent = false;
