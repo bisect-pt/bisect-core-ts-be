2050683bb38e4297190771ee695a3f322d28a218
Pedro Ferreira <pedro@bisect.pt>
2021-05-10 18:04:56 +0100

    20210510 recursive rmdir (#8)
    * Recursive rmdir
    
    * Await rm

--- src/fs.ts
+++ src/fs.ts
@@ -19,7 +19,7 @@ export async function recreateDirectory(path: string): Promise<void> {
         const rmOptions: fs.RmDirOptions = {
             recursive: true,
         };
-        fs.promises.rm(path, { recursive: true, force: true });
+        await fs.promises.rm(path, { recursive: true, force: true });
     } catch (e) {
         if (e.code !== 'ENOENT') {
             // The error is not file does not exist.
